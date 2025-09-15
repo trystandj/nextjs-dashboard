f3168760f1a1ae75ff600fb4d9620401206e4a01
trystandj <trystandj@gmail.com>
2025-09-15 19:22:15 -0400

    fixed fonts

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import styles from '@/app/ui/home.module.css';
 import Link from 'next/link';
-import { inter, lusitana } from '@/app/ui/font';
+import { inter, lusitana } from '@/app/ui/fonts';
 import Image from 'next/image';
 
 export default function Page() {
