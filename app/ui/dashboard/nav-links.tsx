67df39427fc380003e40828d7e53e602fd3d77ea
trystandj <trystandj@gmail.com>
2025-09-17 19:52:06 -0400

    Up to chapter 16 has been completed

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -1,4 +1,5 @@
 'use client';
+
 import {
   UserGroupIcon,
   HomeIcon,
@@ -21,16 +22,17 @@ const links = [
 ];
 
 export default function NavLinks() {
-    const pathname = usePathname();
+  const pathname = usePathname();
+
   return (
     <>
       {links.map((link) => {
         const LinkIcon = link.icon;
         return (
-         <Link
+          <Link
             key={link.name}
             href={link.href}
-                 className={clsx(
+            className={clsx(
               'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
               {
                 'bg-sky-100 text-blue-600': pathname === link.href,
